d541a22666ba706d8a4e536565d2347b12938111
gurjarvicky109 <45270992+gurjarvicky109@users.noreply.github.com>
2018-11-23 13:28:09 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const ms =require("ms");
+const os = require("os-utils");
+const moment = require("moment");
+const Discord = require("discord.js");
+const duration = require("moment-duration-format");
+
+const config = require("./config.json");
+const client = new Discord.Client();
+
+// console log.(NTE0NDYzOTMxOTg1NjkwNjI3.DtlECw.Gggji4HESND84zgfWFZFHJFdceA);
